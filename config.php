911e987dac0710a553fda15da5f08d6fdf626a61
Mobin-ur-Rehman <mobinurrehman@gmail.com>
2020-04-04 22:17:00 +0500

    Editing by Mobin

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+This file contains database configuration assuming you are running mysql using user "root" and password ""
+*/
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'pp');
+
+// Try connecting to the Database
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,8 @@
+All copyright has been reserved by Easyway Agencies.
+This is E-Learning website made for Parallel Pacifico.
+
+Developers:
+
+Muhammad Taha Azam   (https://www.linkedin.com/in/muhammad-taha-azam-1a210b158/)
+Syed Mobin ur Rehman
+ 
